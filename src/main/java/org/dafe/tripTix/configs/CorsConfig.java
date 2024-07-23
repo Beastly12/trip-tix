2cfefe29947b34ff8770272ed851ea2557628e70
Ebube Uzor <ebubeuzor17@gmail.com>
2024-07-23 19:00:34 +0100

    Using the actual domain 3

--- src/main/java/org/dafe/tripTix/configs/CorsConfig.java
+++ src/main/java/org/dafe/tripTix/configs/CorsConfig.java
@@ -7,35 +7,59 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Collections;
 import java.util.List;
 
 
+@Configuration
 public class CorsConfig {
     private static final Long MAX_AGE_SECS = 3600L;
 
-    @Value("#{'${cors.exposed-headers}'.split(',')}")
-    public List<String> exposedHeaders;
+    @Value("${cors.expose-headers}")
+    private String exposeHeaders;
 
-    @Value("#{'${cors.allowed-methods}'.split(',')}")
-    public List<String> allowedMethods;
+    @Value("${cors.allowed-methods}")
+    private String allowedMethods;
 
-    @Value("#{'${cors.allowed-headers}'.split(',')}")
-    public List<String> allowedHeaders;
+    @Value("${cors.allowed-headers}")
+    private String allowedHeaders;
 
-    @Value("#{'${cors.allowed-origins}'.split(',')}")
-    public List<String> allowedOrigins;
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
 
     @Bean
     public CorsConfigurationSource configurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(allowedOrigins);
-        configuration.setAllowedHeaders(allowedHeaders);
-        configuration.setAllowedMethods(allowedMethods);
-        configuration.setExposedHeaders(exposedHeaders);
+
+        if ("*".equals(allowedOrigins)) {
+            configuration.setAllowedOrigins(Collections.singletonList("*"));
+        } else {
+            configuration.setAllowedOrigins(List.of(allowedOrigins.split(",")));
+        }
+
+        if ("*".equals(allowedMethods)) {
+            configuration.setAllowedMethods(Collections.singletonList("*"));
+        } else {
+            configuration.setAllowedMethods(List.of(allowedMethods.split(",")));
+        }
+
+        if ("*".equals(allowedHeaders)) {
+            configuration.addAllowedHeader("*");
+        } else {
+            configuration.setAllowedHeaders(List.of(allowedHeaders.split(",")));
+        }
+
+        if ("*".equals(exposeHeaders)) {
+            configuration.setExposedHeaders(Collections.singletonList("*"));
+        } else {
+            configuration.setExposedHeaders(List.of(exposeHeaders.split(",")));
+        }
+
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(MAX_AGE_SECS);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);  // Changed to cover all paths
+        source.registerCorsConfiguration("/**", configuration);  // Covers all paths
         return source;
     }
 }
